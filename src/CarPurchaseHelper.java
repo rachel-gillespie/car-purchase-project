433a159fe4159eb4fed8e3f129576da660b8618a
Rachel Gillespie <gillespierachel74@gmail.com>
2025-11-01 16:33:41 +0000

    Made changes to CarPurchaseProject

--- src/CarPurchaseHelper.java
+++ src/CarPurchaseHelper.java
@@ -13,87 +13,88 @@ public class CarPurchaseHelper { // contains logic, tasks and reusable functiona
         double monthlyInterestRate = (annualInterest / 100) / 12; // declares a variable called monthlyInterestRate of type double and assigns it the value of annualInterest divided by 100, then divided by 12.
         int totalMonths = termYears * 12; // declares a variable called totalMonths of type int and assigns it the value of termYears multiplied by 12.
 
+        double monthlyPayment; // declares a variable called monthlyPayment of type double.
+
         if (monthlyInterestRate == 0) { // Conditional statement. If monthlyInterestRate is 0.
-            return amountToFinance / totalMonths; // return amountToFinance divided by totalMonths if condition1 gave a true result.
-        } else { // Do these statements if condition1 gave a false result.
+            monthlyPayment = amountToFinance / totalMonths; // Variable monthlyPayment is assigned amountToFinance divided by totalMonths if condition gave a true result.
+        } else { // Do these statements if condition gave a false result.
             // Calculate (1 + r)^n using a loop
             double factor = 1; // declares a variable called factor of type double and assigns it the value of 1.
-            for (int i = 0; i < totalMonths; i++) { // for loop. Initialize int i the value of 0. Set boolean condition, if i is less than totalMonths. Post-body action, i increment.
+            for (int i = 0; i < totalMonths; i++) { // for loop. Initialize int i the value of 0. Set boolean condition, if i is less than totalMonths. Post-body action, increment i.
                 factor *= (1 + monthlyInterestRate); // factor is factor multiplied by 1 added to monthlyInterestRate.
             }
 
-            double monthlyPayment = amountToFinance * monthlyInterestRate * factor / (factor - 1); // declares a variable called monthlyPayment of type double and assigns it the value of amountToFinance multiplied by monthlyInterestRate, multiplied by factor and then divided by factor minus 1.
-            return monthlyPayment; // returns monthlyPayment if condition1 gave a false result.
+            monthlyPayment = amountToFinance * monthlyInterestRate * factor / (factor - 1); // Variable monthlyPayment is assigned the value of amountToFinance multiplied by monthlyInterestRate, multiplied by factor and then divided by factor minus 1.
         }
+
+        return monthlyPayment; // returns monthlyPayment
     }
 
     // ToDO: explain it in the reflection
     public double pcpResultMonth(double carPrice, double deposit, double annualInterest, int termYears, double gmfvPercent) { // pcpResultMonth method, returns a double, has 5 parameters.
-        double gmfv = carPrice * (gmfvPercent / 100);
-        double amountToFinance = carPrice - deposit - gmfv;
+        double gmfv = carPrice * (gmfvPercent / 100); // declares a variable called gmfv of type double and assigns it the value of gmfvPercent divided by 100, then multiplied by carPrice.
+        double amountToFinance = carPrice - deposit - gmfv; // declares a variable called amountToFinance of type double and assigns it the value of carPrice minus deposit minus gmfv.
 
-        double monthlyInterestRate = (annualInterest / 100) / 12;
-        int totalMonths = termYears * 12;
+        double monthlyInterestRate = (annualInterest / 100) / 12; // declares a variable called monthlyInterestRate of type double and assigns it the value of annualInterest divided by 100, then divided by 12.
+        int totalMonths = termYears * 12; // declares a variable called totalMonths of type int and assigns it the value of termYears multiplied by 12.
 
-        double monthlyPayment;
+        double monthlyPayment; // declares a variable called monthlyPayment of type double.
 
-        if (monthlyInterestRate == 0) {
-            monthlyPayment = amountToFinance / totalMonths;
-        } else {
+        if (monthlyInterestRate == 0) { // Conditional Statement. If monthlyInterestRate is 0.
+            monthlyPayment = amountToFinance / totalMonths; // Variable monthlyPayment is assigned amountToFinance divided by totalMonths if condition gave a true result.
+        } else { // Do these statements if condition gave a false result.
             // Calculate (1 + r)^n using a loop
-            double factor = 1;
-            for (int i = 0; i < totalMonths; i++) {
-                factor *= (1 + monthlyInterestRate);
+            double factor = 1; // declares a variable called factor of type double and assign it the value of 1.
+            for (int i = 0; i < totalMonths; i++) { //  for loop. Initialize int i the value of 0. Set boolean condition, if i is less than totalMonths. Post-body action, i increment.
+                factor *= (1 + monthlyInterestRate); // factor is factor multiplied by 1 added to monthlyInterestRate.
             }
 
-            monthlyPayment = amountToFinance * monthlyInterestRate * factor / (factor - 1);
+            monthlyPayment = amountToFinance * monthlyInterestRate * factor / (factor - 1); // Variable monthlyPayment is assigned amountToFinance multiplied by monthlyInterestRate, multiplied by factor and then divided by factor minus 1.
         }
 
-        return monthlyPayment;
+        return monthlyPayment; // returns monthlyPayment
     }
 
     public double fuelCostEstimator(double weeklyKilometers, double fuelCost) { // fuelCostEstimator method, returns a double, has 2 parameters.
 
-        double weeklyFuelCost;
+        double weeklyFuelCost; // declares a variable weeklyFuelCost of type double.
 
-        weeklyFuelCost = (weeklyKilometers / 100) * 6 * fuelCost; // assume 6 l per 100 km.
+        weeklyFuelCost = (weeklyKilometers / 100) * 6 * fuelCost; // assume 6 l per 100 km. Variable weeklyFuelCost is assigned the value of weekly kilometers divided by 100, then multiplied by 6, then multiplied by fuelCost.
 
-        return weeklyFuelCost;
+        return weeklyFuelCost; // returns weeklyFuelCost
     }
 
     public double calculateMonthlyCost(double carPrice, int termMonths) { // calculateMonthlyCost method, returns a double, has 2 parameters.
 
-        double monthlyPayment;
+        double monthlyPayment; // declares a variable monthlyPayment of type double.
 
-        monthlyPayment = carPrice / termMonths;
+        monthlyPayment = carPrice / termMonths; // Variable monthlyPayment is assigned the value of carPrice divided by termMonths.
 
-        return monthlyPayment;
+        return monthlyPayment; // returns monthlyPayment
     }
 
-    public String recommendCarType(String carRequirement) { // recommendCarType method, returns a string, has 1 requirement
+    public String recommendCarType(String carRequirement) { // recommendCarType method, returns a string, has 1 parameter.
 
-        if (carRequirement.equalsIgnoreCase("family")) {
-            return "Skoda Octavia Estate";
-        } else if (carRequirement.equalsIgnoreCase("commuter")) {
-            return "Toyota Corolla Hybrid";
-        } else if (carRequirement.equalsIgnoreCase("adventure")) {
-            return "Jeep Wrangler Rubicon";
-        } else {
-            return "You did not input a valid requirement!";
+        if (carRequirement.equalsIgnoreCase("family")) { // checking if the string inputted is equal to family, ignoring letter case.
+            return "Skoda Octavia Estate"; // if the above statement is true, return this string.
+        } else if (carRequirement.equalsIgnoreCase("commuter")) { // checking if the string inputted is equal to commuter, ignoring letter case.
+            return "Toyota Corolla Hybrid"; // if the above statement is true, return this string.
+        } else if (carRequirement.equalsIgnoreCase("adventure")) { // checking if the string inputted is equal to adventure, ignoring letter case.
+            return "Jeep Wrangler Rubicon"; // if the above statement is true, return this string.
+        } else { // if the above statements are false
+            return "You did not input a valid requirement!"; // if the above statements are false, return this string.
         }
     }
 
-    public boolean returnToMenu(String reply) {
-        if (reply.equalsIgnoreCase("yes")) {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean returnToMenu(String reply) { // returnToMenu method, returns a boolean, has 1 parameter.
+        return reply.equalsIgnoreCase("yes"); // checking if the string inputted is equal to yes, ignoring letter case.
     }
 
     public String printGoodbyeMessage() { // printGoodbyeMessage method, returns a string, has no parameters.
 
         return "We hope you had a successful experience! Goodbye!"; // Returns String
     }
+
+
 }
 
--- src/Driver.java
+++ src/Driver.java
@@ -1,22 +1,23 @@
 // Rachel Gillespie, 20118715, Car Helper Program to give users useful information and advice when choosing their next car.
 
-import java.util.Scanner; // Import Scanner
+import java.util.Scanner; // Import Scanner.
 
 public class Driver {
     public static void main(String[] args) {// contains main method and runs the program.
 
-        new Driver();
+        new Driver(); // creates an instance of driver.
     }
+
     Driver() {
 
         Scanner input = new Scanner(System.in); // Created a Scanner Object called input and set input as keyboard.
 
-        CarPurchaseHelper carPurchaserHelper = new CarPurchaseHelper(); // Created a CarPurchaseHelper Object called carPurchaserHelper
+        CarPurchaseHelper carPurchaserHelper = new CarPurchaseHelper(); // Created a CarPurchaseHelper Object called carPurchaserHelper.
 
-        System.out.println(carPurchaserHelper.printWelcomeMessage()); // Calls Welcome message
+        System.out.println(carPurchaserHelper.printWelcomeMessage()); // Calls Welcome message.
 
         int choice = 0;
-        while (choice != 6) {// for loop repeats while 'i' is less than or equal to 6.
+        while (choice != 6) { // while loop repeats while choice does not equal 6.
 
             System.out.println("  ____             _   _      _                 \n" +
                     " / ___|__ _ _ __  | | | | ___| |_ __   ___ _ __ \n" +
@@ -36,17 +37,17 @@ public class Driver {
             System.out.println("5. Recommend Car Type");
             System.out.println("6. Exit Program");
             System.out.println(">>> Choose an option (1-6): ");
-            choice = input.nextInt();
+            choice = input.nextInt(); // user inputs choice.
 
             System.out.println("_______________________________________________________________________");
 
-            if (choice == 1) { // if user chooses 1. Calculate HP
+            if (choice == 1) { // if user chooses 1. Calculate HP.
 
                 // --- HP Calculation ---
                 System.out.println("--- Main Menu > Calculate HP ---");
-                System.out.print("Enter car price: ");
+                System.out.print("Enter car price: €");
                 double hpPrice = input.nextDouble();
-                System.out.print("Enter deposit: ");
+                System.out.print("Enter deposit: €");
                 double hpDeposit = input.nextDouble();
                 System.out.print("Enter annual interest rate (%): ");
                 double hpInterest = input.nextDouble();
@@ -54,25 +55,23 @@ public class Driver {
                 int hpTerm = input.nextInt();
 
                 double hpMonthly = carPurchaserHelper.calculateHP(hpPrice, hpDeposit, hpInterest, hpTerm);
-                System.out.println("Monthly HP payment: €" + hpMonthly);
+                System.out.println("Monthly HP payment: €" + String.format("%.2f", hpMonthly)); //formats hpMonthly to 2 decimal places.
 
-                System.out.println("Return to menu");
-                input.nextLine();
+                System.out.println("Return to menu(yes/no): ");
                 String reply = input.nextLine();
-                if (carPurchaserHelper.returnToMenu(reply)) {
-                    choice = 0;
-                } else {
-
-                    System.out.println("_______________________________________________________________________");
+                if (!carPurchaserHelper.returnToMenu(reply)) {
+                    choice = 6; // if user doesn't input yes, the program prints the goodbye message and ends.
                 }
 
-            } else if (choice == 2) { // if user chooses 2. Calculate PCP
+                System.out.println("_______________________________________________________________________");
+
+            } else if (choice == 2) { // if user chooses 2. Calculate PCP.
 
                 // --- PCP Calculation ---
                 System.out.println("--- Main Menu > Calculate PCP ---");
-                System.out.print("Enter car price: ");
+                System.out.print("Enter car price: €");
                 double pcpPrice = input.nextDouble();
-                System.out.print("Enter deposit: ");
+                System.out.print("Enter deposit: €");
                 double pcpDeposit = input.nextDouble();
                 System.out.print("Enter annual interest rate (%): ");
                 double pcpInterest = input.nextDouble();
@@ -82,25 +81,37 @@ public class Driver {
                 double gmfvPercent = input.nextDouble();
 
                 double pcpMonthly = carPurchaserHelper.pcpResultMonth(pcpPrice, pcpDeposit, pcpInterest, pcpTerm, gmfvPercent);
-                System.out.println("Monthly PCP payment: €" + pcpMonthly);
+                System.out.println("Monthly PCP payment: €" + String.format("%.2f", pcpMonthly));
+
+                System.out.println("Return to menu(yes/no): ");
+                String reply = input.nextLine();
+                if (!carPurchaserHelper.returnToMenu(reply)) {
+                    choice = 6; // if user doesn't input yes, the program prints the goodbye message and ends.
+                }
 
                 System.out.println("_______________________________________________________________________");
 
-            } else if (choice == 3) { // if user chooses 3. Calculate Fuel Cost
+            } else if (choice == 3) { // if user chooses 3. Calculate Fuel Cost.
 
                 // --- Fuel Cost Calculation ---
                 System.out.println("--- Main Menu > Calculate Fuel Cost ---");
                 System.out.print("Enter weekly kilometres: ");
                 double fuelDistance = input.nextDouble();
-                System.out.print("Enter fuel cost: ");
+                System.out.print("Enter fuel cost (per litre): €");
                 double fuelCost = input.nextDouble();
 
                 double fuelWeekly = carPurchaserHelper.fuelCostEstimator(fuelDistance, fuelCost);
-                System.out.println("Weekly Fuel Cost: €" + fuelWeekly);
+                System.out.println("Weekly Fuel Cost: €" + String.format("%.2f", fuelWeekly));
+
+                System.out.println("Return to menu(yes/no): ");
+                String reply = input.nextLine();
+                if (!carPurchaserHelper.returnToMenu(reply)) {
+                    choice = 6; // if user doesn't input yes, the program prints the goodbye message and ends.
+                }
 
                 System.out.println("_______________________________________________________________________");
 
-            } else if (choice == 4) { // if user chooses 4. Calculate Monthly Cost
+            } else if (choice == 4) { // if user chooses 4. Calculate Monthly Cost.
 
                 // --- Monthly Cost Calculation ---
                 System.out.println("--- Main Menu > Calculate Monthly Cost ---");
@@ -110,11 +121,17 @@ public class Driver {
                 int monthTerm = input.nextInt();
 
                 double costMonthly = carPurchaserHelper.calculateMonthlyCost(carPrice, monthTerm);
-                System.out.println("Monthly Cost: €" + costMonthly);
+                System.out.println("Monthly Cost: €" + String.format("%.2f", costMonthly));
+
+                System.out.println("Return to menu(yes/no): ");
+                String reply = input.nextLine();
+                if (!carPurchaserHelper.returnToMenu(reply)) {
+                    choice = 6; // if user doesn't input yes, the program prints the goodbye message and ends.
+                }
 
                 System.out.println("_______________________________________________________________________");
 
-            } else if (choice == 5) { // if user chooses 5. Recommend Car Type
+            } else if (choice == 5) { // if user chooses 5. Recommend Car Type.
 
                 input.nextLine();
 
@@ -123,24 +140,42 @@ public class Driver {
                 System.out.println("Enter car requirement, (e.g. family, commute, adventure): ");
                 String carRequirement = input.nextLine();
 
+                while (!(carRequirement.equalsIgnoreCase("family")) && !(carRequirement.equalsIgnoreCase("commute")) && !(carRequirement.equalsIgnoreCase("adventure"))) { // Checking if input is not equal to these 3.
+
+                    System.out.println("Invalid input!");
+
+                    System.out.println("Enter car requirement, (e.g. family, commute, adventure): ");
+                    carRequirement = input.nextLine();
+
+                }
+
                 String RecommendCar = carPurchaserHelper.recommendCarType(carRequirement);
                 System.out.println("Recommended Car: " + RecommendCar);
 
+                System.out.println("Return to menu(yes/no): ");
+                String reply = input.nextLine();
+                if (!carPurchaserHelper.returnToMenu(reply)) {
+                    choice = 6; // if user doesn't input yes, the program prints the goodbye message and ends.
+                }
+
                 System.out.println("_______________________________________________________________________");
 
-            } else if (choice == 6) {
-
-                // --- Exit ---
-                System.out.println(carPurchaserHelper.printGoodbyeMessage()); // Calls Goodbye message
-
-                System.out.println("_______________________________________________________________________");
+            } else if (choice == 6) { // Doesn't print Error message if choice is 6.
 
             } else {
+                
                 System.out.println("Error: Invalid choice. Please enter a number between 1 & 6.");
 
                 System.out.println("_______________________________________________________________________");
             }
+
         }
+
+        // --- Exit ---
+        System.out.println(carPurchaserHelper.printGoodbyeMessage()); // Calls Goodbye message.
+
+        System.out.println("_______________________________________________________________________");
+
     }
 
 }
